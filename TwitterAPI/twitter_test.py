f9c407c0f2b33c214bc8c7e3264d8b2be48712aa
trasowech_swd <trasowech@uiowa.edu>
2018-10-06 14:18:12 -0500

    changed to instance variables

--- TwitterAPI/twitter_test.py
+++ TwitterAPI/twitter_test.py
@@ -2,21 +2,21 @@ import os
 from json import load
 from twython import TwythonStreamer
 import csv
-
+import time
 
 # Filter out unwanted data
 def process_tweet(tweet):
     d = {}
-    # d['hashtags'] = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
     d['text'] = tweet['text']
-    # d['user'] = tweet['user']['screen_name']
-    # d['user_loc'] = tweet['user']['location']
+    d['time'] = tweet['created_at']
     return d
 
 
 # Create a class that inherits TwythonStreamer
 class MyStreamer(TwythonStreamer):
     def __init__(self, settings_file: str = '../res/twitter_api_credentials.json'):
+        self.count = 0
+        self.tweet_array = []
         with open(settings_file, 'r', encoding='utf-8') as settings_data:
             settings = load(settings_data)
             # twitter api settings
@@ -25,10 +25,7 @@ class MyStreamer(TwythonStreamer):
             self.ACCESS_TOKEN = settings['ACCESS_TOKEN']
             self.ACCESS_SECRET = settings['ACCESS_SECRET']
         super().__init__(self.CONSUMER_KEY, self.CONSUMER_SECRET, self.ACCESS_TOKEN, self.ACCESS_SECRET)
-    count = 0
-    tweet_array = []
 
-    # os.remove('saved_tweets.csv');
 
     def on_success(self, data):
         # Only collect tweets in English
@@ -50,16 +47,16 @@ class MyStreamer(TwythonStreamer):
         #  writer.writerow(list(var))
 
     def fill_tweet_array(self, tweet):
-        if MyStreamer.count < 100:
-            MyStreamer.count += 1
+        if self.count < 100:
+            self.count += 1
             tweet_values = tweet.values()
             if len(str(tweet_values)) > 50:
-                MyStreamer.tweet_array.append(tweet.values())
+                self.tweet_array.append(tweet.values())
         else:
             MyStreamer.count = 0
             with open(r'saved_tweets.csv', 'w', encoding='utf-8') as file:
                 writer = csv.writer(file)
-                for row in MyStreamer.tweet_array:
+                for row in self.tweet_array:
                     writer.writerow(row)
 
 
