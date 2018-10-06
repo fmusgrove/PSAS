917842b216e5bdf9e16ce6b93b0e6b110b8e56b0
trasowech_swd <trasowech@uiowa.edu>
2018-10-06 14:32:47 -0500

    changed to instance variables

--- PSASAgent.py
+++ PSASAgent.py
@@ -1,5 +1,6 @@
 import atexit
 from Database.db_interface import DBInterface
+from TwitterAPI.twitter_test import MyStreamer
 
 
 def exit_handler():
@@ -16,8 +17,9 @@ if __name__ == '__main__':
     # Creating the DBInterface object will open the ssh tunnel, must be closed before exiting
     db = DBInterface('res/ssh_db_pw.json')
     rows = db.run_command("SELECT * FROM twitter").fetchall()
-    for row in rows:
-        for col in row:
-            print(col)
-    print(str(db))
+    db.run_command()
+    # Instantiate from our streaming class
+    stream = MyStreamer()
+    # Start the stream
+    stream.statuses.filter(track='trump')
     db.close()
--- TwitterAPI/twitter_test.py
+++ TwitterAPI/twitter_test.py
@@ -2,21 +2,19 @@ import os
 from json import load
 from twython import TwythonStreamer
 import csv
-import time
+
 
 # Filter out unwanted data
 def process_tweet(tweet):
-    d = {}
-    d['text'] = tweet['text']
-    d['time'] = tweet['created_at']
-    return d
+    return {'text': tweet['text'], 'time': tweet['created_at']}
 
 
 # Create a class that inherits TwythonStreamer
 class MyStreamer(TwythonStreamer):
-    def __init__(self, settings_file: str = '../res/twitter_api_credentials.json'):
+    def __init__(self, settings_file: str = '../res/twitter_api_credentials.json',database_interface=None):
         self.count = 0
         self.tweet_array = []
+        self.database_interface=database_interface
         with open(settings_file, 'r', encoding='utf-8') as settings_data:
             settings = load(settings_data)
             # twitter api settings
@@ -25,7 +23,6 @@ class MyStreamer(TwythonStreamer):
             self.ACCESS_TOKEN = settings['ACCESS_TOKEN']
             self.ACCESS_SECRET = settings['ACCESS_SECRET']
         super().__init__(self.CONSUMER_KEY, self.CONSUMER_SECRET, self.ACCESS_TOKEN, self.ACCESS_SECRET)
-
 
     def on_success(self, data):
         # Only collect tweets in English
@@ -38,13 +35,6 @@ class MyStreamer(TwythonStreamer):
     def on_error(self, status_code, data):
         print(status_code, data)
         self.disconnect()
-
-    # Save each tweet to csv file
-    def save_to_csv(self, tweet):
-        var = tweet.values()
-        print(var)
-
-        #  writer.writerow(list(var))
 
     def fill_tweet_array(self, tweet):
         if self.count < 100:
@@ -60,7 +50,3 @@ class MyStreamer(TwythonStreamer):
                     writer.writerow(row)
 
 
-# Instantiate from our streaming class
-stream = MyStreamer()
-# Start the stream
-stream.statuses.filter(track='trump')
