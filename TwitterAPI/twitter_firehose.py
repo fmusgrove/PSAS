7d7382a9036b5272279f94cf1dc3d4d1964dafdb
forrestmusgrove <forrest-musgrove@uiowa.edu>
2018-10-06 21:20:45 -0500

    Prepared TwitterFirehose for multiprocessing

---
+++ TwitterAPI/twitter_firehose.py
@@ -0,0 +1,75 @@
+from json import load
+from twython import TwythonStreamer
+from unidecode import unidecode
+from Utility.connected_process import ConnectedProcess
+
+
+# Create a class that inherits TwythonStreamer
+class TwitterFirehose(TwythonStreamer, ConnectedProcess):
+    def __init__(self, settings_file: str = '../res/twitter_api_credentials.json', feature='twitter',
+                 database_interface=None,
+                 message_pipe=None):
+        with open(settings_file, 'r', encoding='utf-8') as settings_data:
+            settings = load(settings_data)
+            # twitter api settings
+            self.CONSUMER_KEY = settings['CONSUMER_KEY']
+            self.CONSUMER_SECRET = settings['CONSUMER_SECRET']
+            self.ACCESS_TOKEN = settings['ACCESS_TOKEN']
+            self.ACCESS_SECRET = settings['ACCESS_SECRET']
+        super(TwitterFirehose).__init__(self.CONSUMER_KEY, self.CONSUMER_SECRET, self.ACCESS_TOKEN, self.ACCESS_SECRET)
+        super(TwitterFirehose).__init__(name='PSAS Twitter Firehose', database_interface=database_interface,
+                                        message_pipe=message_pipe)
+        self.count = 0
+        self.tweet_array = []
+        self.database_interface = database_interface
+        self.feature = feature
+
+    def _message_listener(self):
+        msg = ''
+        while not msg == 'EXIT':
+            msg = self.message_pipe['pipe_out'].recv()
+            # Handle messages here
+
+        self.should_exit = True
+        self._close()
+
+    def _start_work(self):
+        """
+        Main entry point for new process, ran on separate thread from _message_listener
+        """
+        self.statuses.filter(track=self.feature)
+
+    def _close(self):
+        """
+        Called after exit poison pill is received
+        """
+        self.disconnect()
+
+    # Filter out unwanted data
+    def process_tweet(self, tweet):
+        """
+        Return only tweet text and timestamp. Checks for truncation and returns full text
+        :param tweet: tweet text string to process
+        :return: dict(text, time)
+        """
+        if tweet['truncated']:
+            data = unidecode(tweet['extended_tweet']['full_text'])
+        else:
+            data = unidecode(tweet['text'])
+        return {'text': data, 'time': unidecode(tweet['created_at'])}
+
+    def on_success(self, data):
+        # Only collect tweets in English
+        if 'lang' in data:
+            if data['lang'] == 'en':
+                tweet_data = self.process_tweet(data)
+                self.fill_tweet_array(tweet_data)
+
+    def on_error(self, status_code, data):
+        print(status_code, data)
+        # self.disconnect()
+
+    def fill_tweet_array(self, tweet):
+        # self.database_interface.run_command("INSERT INTO temp_tweet_table(time,tweet) VALUES (%s,%s)",
+        #                                     (tweet['time'], tweet['text']), should_return=False)
+        pass
